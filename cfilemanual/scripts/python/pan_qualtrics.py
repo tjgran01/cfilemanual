4a204c32076b4cd8a6bc4cc3dfb6d1c7509b1e4f
tjgran01 <tjgran01@syr.edu>
2018-03-17 11:29:08 -0400

    Updated pan_qyaltrics.py to begin working on autogen of conditions files.

--- cfilemanual/scripts/python/pan_qualtrics.py
+++ cfilemanual/scripts/python/pan_qualtrics.py
@@ -3,30 +3,70 @@ import os
 import getpass
 
 import get_qualtrics
+from inputmanager import InputManager
 
-# cols not needed in qualtrics export.
-col_to_drop = ["ResponseID", "ResponseSet", "IPAddress", "StartDate", "EndDate",
-	           "RecipientLastName",	"RecipientFirstName", "RecipientEmail",
-               "ExternalDataReference",	"Finished", "Status", "LocationLatitude",
-               "LocationLongitude",	"LocationAccuracy"]
+def check_if_download():
+	if not os.path.exists(f"{os.getcwd()}/MyQualtricsDownload/"):
+		apiToken = get_qualtrics.get_api_token()
+		surveyId = get_qualtrics.get_survey_id(apiToken)
+		get_qualtrics.main(surveyId, apiToken)
 
-if not os.path.exists(f"{os.getcwd()}/MyQualtricsDownload/"):
-	apiToken = get_qualtrics.get_api_token()
-	surveyId = get_qualtrics.get_survey_id(apiToken)
-	get_qualtrics.main(surveyId, apiToken)
+def get_experiment_id(par_id):
 
-file_name = str(os.listdir(f"{os.getcwd()}/MyQualtricsDownload/"))[2:-2]
-df = pd.read_csv(f"{os.getcwd()}/MyQualtricsDownload/{file_name}")
-df.drop(col_to_drop, axis=1, inplace=True)
-df.drop(1, axis=0, inplace=True)
+	exper_id = par_id[:2]
+	return exper_id
 
-print(df)
+def make_c_files(par_ids):
+	par_ids.pop(0)
+	exper_id = get_experiment_id(par_ids[0])
 
-# num_participants = df.shape[0] - 2
-#
-# for i, row in df.iterrows():
-#     if i > 1:
-#         participant_id = row[0]
-#         print(participant_id)
-#
-# print(num_participants)
+	if not os.path.exists(f"./experiment_{exper_id}_cfiles/"):
+		os.mkdir(f"./experiment_99{exper_id}_cfiles/")
+
+	for par_id in par_ids:
+		with open(f"./experiment_99{exper_id}_cfiles/{par_id}_FNIRS_conditions_s1.csv", "w") as out_csv:
+			out_csv.write()
+
+def main(col_to_drop, slice_prompt):
+	check_if_download()
+	file_name = str(os.listdir(f"{os.getcwd()}/MyQualtricsDownload/"))[2:-2]
+	df = pd.read_csv(f"{os.getcwd()}/MyQualtricsDownload/{file_name}")
+	df.drop(col_to_drop, axis=1, inplace=True)
+	df.drop(1, axis=0, inplace=True)
+
+	par_ids = df["enter_par_id"].tolist()
+	num_participants = df.shape[0] - 2
+
+	# sets participant id as index of df.
+	df.set_index("enter_par_id", inplace=True)
+
+	indexer_list = df.loc["(For Experimenter) Please Enter Participant ID."].tolist()
+
+	slice_points = []
+	for i, row in enumerate(indexer_list):
+		if row == slice_prompt:
+			slice_points.append(i)
+
+	list_of_tasks = []
+	for index, row in df.iterrows():
+		if index.isnumeric():
+			list_row = row.tolist()
+			task_list = []
+			for i, cell in enumerate(list_row):
+				for val in slice_points:
+					if i == val:
+						print(cell)
+
+
+	# make_c_files(par_ids)
+
+
+if __name__ == "__main__":
+	# cols not needed in qualtrics export.
+	col_to_drop = ["ResponseID", "ResponseSet", "IPAddress", "StartDate",
+				   "EndDate", "RecipientLastName",	"RecipientFirstName",
+				   "RecipientEmail", "ExternalDataReference", "Finished",
+				   "Status", "LocationLatitude", "LocationLongitude",
+				   "LocationAccuracy"]
+	slice_prompt = "(For Experimenter) Please select the corresponding task number referring to the task the particip..."
+	main(col_to_drop, slice_prompt)
