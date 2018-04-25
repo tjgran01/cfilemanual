ed1df4822aef633858d0f3f0e7cd1fb59836b241
mindlab <tjgran01@syr.edu>
2018-04-25 09:51:55 -0400

    Pulled from remote to lab machine.

--- cfilemanual/scripts/python/cfile_test_2.py
+++ cfilemanual/scripts/python/cfile_test_2.py
@@ -1,5 +1,5 @@
-from qualtricsparser import QualtricsParser
+import os
 
-q = QualtricsParser(cond_file="./MyQualtricsDownload/cfile_test_2.csv")
-q.parse_at_marks()
-q.parse_marked_data()
+something = []
+for file in os.getcwd():
+    
